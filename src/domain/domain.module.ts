fefb726d6efade4340cbc021f20ed321fee34e38
githiago-f <tfarias@protonmail.com>
2021-06-26 17:51:26 -0300

    ref: remove controllers from domain module

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ProfessionalsController } from './professional/controller/ProfessionalsController';
-import { ProfessionalTypeController } from './professional/controller/ProfessionalTypeController';
+import { DatabaseModule } from 'src/data/database.module';
+import { ProfessionalSubscriber } from './professional/entity/subscribers/ProfessionalSubscriber';
+import { ProfessionalTypeSubscriber } from './professional/entity/subscribers/ProfessionalTypeSubscriber';
+import { ProfessionalRepository } from './professional/repository/ProfessionalRepository';
+import { ProfessionalTypeRepository } from './professional/repository/ProfessionalTypeRepository';
+import { ProfessionalService } from './professional/service/ProfessionalService';
+import { ProfessionalTypeService } from './professional/service/ProfessionalTypeService';
 
 @Module({
-  imports: [],
-  controllers: [
-    ProfessionalsController,
-    ProfessionalTypeController
+  imports: [ DatabaseModule ],
+  providers: [
+    ProfessionalTypeRepository,
+    ProfessionalRepository,
+    ProfessionalService,
+    ProfessionalTypeService,
+    ProfessionalSubscriber,
+    ProfessionalTypeSubscriber
   ],
-  providers: [],
+  exports: [
+    ProfessionalService,
+    ProfessionalTypeService
+  ]
 })
 export class DomainModule { }
